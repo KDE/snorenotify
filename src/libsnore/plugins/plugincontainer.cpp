8dc8bad34e828c44ecd089004227a458fe5ece87
Patrick von Reth <vonreth@kde.org>
2015-07-28 00:50:29 +0200

    fix mac and qt5 linux

--- src/libsnore/plugins/plugincontainer.cpp
+++ src/libsnore/plugins/plugincontainer.cpp
@@ -139,7 +139,7 @@ const QDir &PluginContainer::pluginDir()
         QString appDir = qApp->applicationDirPath();
         QStringList list;
 #ifdef Q_OS_MAC
-        if (appDir == "MacOS") {
+        if (appDir == QStringList("MacOS")) {
             list << appDir;
             QDir dir(appDir);
             // Development convenience-hack
