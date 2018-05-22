99c4ad852ca0d532a3d23e8bc630c16013ab5953
phuzion <phuzion@bemaniso.ws>
2018-05-22 19:05:13 +0000

    Remove NMA support, as they are shutting down May 24, 2018

--- src/plugins/secondary_backends/nma/nmaconstants.h
+++
@@ -1,9 +0,0 @@
-#ifndef NMACONSTANTS_H
-#define NMACONSTANTS_H
-
-#include "libsnore/snoreglobals.h"
-
-namespace NMAConstants {
-static const Snore::SettingsKey ApiKey = {QStringLiteral("ApiKey"), Snore::GlobalSetting};
-}
-#endif // NMACONSTANTS_H
